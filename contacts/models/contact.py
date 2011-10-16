7a6ba372426ebaf2b5143206a6df881830a86f41
addressbook <addressbook@mybox.(none)>
2011-10-16 04:45:08 +0200

    Added Schema for custom tables

--- contacts/models/contact.py
+++ contacts/models/contact.py
@@ -5,6 +5,23 @@ import datetime
 
 """
 Model to store contacts
+
+CREATE TABLE `contacts_contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `home_phone` varchar(20) DEFAULT NULL,
+  `mobile_phone` varchar(20) DEFAULT NULL,
+  `office_phone` varchar(20) DEFAULT NULL,
+  `office_phone_extn` varchar(5) DEFAULT NULL,
+  `facebook` varchar(100) DEFAULT NULL,
+  `twitter` varchar(100) DEFAULT NULL,
+  `owner_id` int(11) NOT NULL,
+  `group_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 
 """
 class Contact(Model):
     first_name = CharField(max_length=50, null=False)
--- contacts/models/group.py
+++ contacts/models/group.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 """
 Users can create groups and categorize their contacts
+
+CREATE TABLE `contacts_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `owner_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8
 """
 class Group(Model):
     name = CharField(max_length=100, null=False)
